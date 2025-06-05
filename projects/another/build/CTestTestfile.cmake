361d1a199db1a730316ec5c350ab13f71772f6aa
Nano <marwanpentester@gmail.com>
2025-06-05 21:21:07 +0300

    Third One

--- projects/another/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/sdb1/programming/C++/projects/another
-# Build directory: /mnt/sdb1/programming/C++/projects/another/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
